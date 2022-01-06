f9b7afa47ce2073699a1b296efec895c3a1f429a
The Tophat Demon <tophatdemonproductions@gmail.com>
2022-01-06 14:01:48 -0600

    Players now look towards the mouse and have a randomized hat color based off of name hash.

--- snowball-game/scripts/objects/player.gd
+++ snowball-game/scripts/objects/player.gd
@@ -6,17 +6,23 @@ var label_offset = Vector2()
 
 puppet var puppet_pos = Vector2()
 puppet var puppet_motion = Vector2()
+puppet var puppet_rotation = 0.0
 
 var move_speed = 256.0
 
 var motion = Vector2()
 
+var player_name = "Unnamed"
 var peer_id = 0
 
 func _ready():
 	var _err = get_tree().connect("network_peer_disconnected", self, "_on_peer_disconnect")
 	puppet_pos = position
-	label.text = Global.players_info[peer_id]["name"]
+	puppet_rotation = rotation
+	player_name = Global.players_info[peer_id]["name"]
+	label.text = player_name
+	
+	$HeadSprite/ColorSprite.modulate = Color(player_name.hash() | 0x000000FF)
 	
 	#Add label to the HUD node so it shows up over everything
 	label_offset = label.rect_position
@@ -41,11 +47,17 @@ func _process(_delta):
 			
 		motion = motion.normalized()
 		
+		#Turn to face the cursor
+		var m_pos = get_global_mouse_position()
+		rotation = atan2(m_pos.y - position.y, m_pos.x - position.x)
+		
 		rset("puppet_motion", motion)
 		rset("puppet_pos", position)
+		rset("puppet_rotation", rotation)
 	else:
 		position = puppet_pos
 		motion = puppet_motion
+		rotation = puppet_rotation
 	
 	#Make name tag follow us
 	label.rect_position = position + label_offset
